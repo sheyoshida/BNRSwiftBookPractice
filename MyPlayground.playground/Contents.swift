0eeff3e4e659b559887c991056fc8aee3dac5069
Shena Yoshida <sheyoshida@gmail.com>
2016-01-18 21:19:41 -0500

    switch statement exercises

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -17,25 +17,22 @@ print(lastName)
 var numberOfStoplights: Int = 10 // variable (var) = mutable type, but must be integer
 numberOfStoplights += 2
 
-let numberOfDogs: Int = 15 // constant (let) = immutable type
+let numberOfDogs: Int = 15 // constant (let) = immutable types
 
 var population: Int // declare empty variable
-population = 4220000
-
-// give it a value later
+population = 4220000 // give it a value
 
 let townName: String = "Kamloops"
 
 var unemploymentRate: Int = 2
 
 let townDescripion = "\(townName) has a population of \(population). There are \(numberOfStoplights) stoplights and \(numberOfDogs) dogs. The unemployment rate is \(unemploymentRate)." // string interpolation
-
 print(townDescripion)
 
 // chapter 3 - conditionals
 
 var message: String
-var hasPostOffice: Bool = true
+var hasPostOffice: Bool = false
 
 if population < 10000 {
     message = "\(population) is a small town!"
@@ -44,7 +41,7 @@ if population < 10000 {
 }
 print(message)
 
-if !hasPostOffice {
+if !hasPostOffice { // is no true
     print("where do we buy stamps?")
 }
 
@@ -63,36 +60,34 @@ if population < 10000 {
 } else {
     message = "\(population) is huge!"
 }
-
 print(message)
-
 
 // chapter 4 - Numbers
 
 print("the maximum Int value is \(Int.max).") // Int = Int64
 print("the minimum Int value is \(Int.min).")
 
-// integers are stored in binary form witha fixed number of bits - a bit is a single 0 or 1. Each bit position represents a power of 2. A regular Int is a 64-bit integer, meaning it has 2 to the power of 64 possible values. Earlier devices were made with a 32-bit architecture. The compiler determines the appropriate size for int wen it builds your program. 
+// integers are stored in binary form with a fixed number of bits - a bit is a single 0 or 1. Each bit position represents a power of 2. A regular Int is a 64-bit integer, meaning it has 2^64 possible values. Earlier devices were made with a 32-bit architecture (2^32). The compiler determines the appropriate size for int wen it builds your program.
 
 print("the maximum value for a 64-bit integer is \(Int64.max).")
-print("the minimum value for a 64-bit integer is \(Int64.max).")
+print("the minimum value for a 64-bit integer is \(Int64.min).")
 
 print("the maximum value for a 32-bit integer is \(Int32.max).")
-print("the minimum value for a 32-bit integer is \(Int32.max).")
+print("the minimum value for a 32-bit integer is \(Int32.min).")
 
 print("the maximum value for a 16-bit integer is \(Int16.max).")
-print("the minimum value for a 16-bit integer is \(Int16.max).")
+print("the minimum value for a 16-bit integer is \(Int16.min).")
 
 print("the maximum value for a 8-bit integer is \(Int8.max).")
-print("the minimum value for a 8-bit integer is \(Int8.max).")
+print("the minimum value for a 8-bit integer is \(Int8.min).")
 
 // all of these types are "signed" - meaning they can be either + or - (ie: NSInteger)
 // you can create a + integer by using UInt (ie: NSUInteger)
 
-print("the maximum value for a UInt integer is \(UInt.max).")
+print("the maximum value for a UInt integer is \(UInt.max).") // Unsigned (+ whole numbers or 0, no negative numbers allowed!)
 print("the minimum value for a UInt integer is \(UInt.max).") // same value as max!
 
-// BEST PRACTICE: use Int for all integer uses (including counts) unless an unsigned integer is required by the algorithm or code you are writing.
+// BEST PRACTICE: use Int for all integer uses (including counts) unless an "unsigned integer" (no negative values) is required by the algorithm or code you are writing.
 
 let numberOfPages: Int = 10 // declare type explicitly
 let numberOfChapters = 3 // type inferred by the compiler
@@ -146,18 +141,19 @@ let c = a + Int16(b) // convert it like this
 
 // floating point numbers (numbers with decimals)
 
-// since double is larger, it is more precise and is the DEFAULT floating-point number in Swift
+// since DOUBLE is larger, it is more precise and is THE DEFAULT floating-point number in Swift
 
 let d1 = 1.1 // implicitly double
 let d2: Double = 1.1 // 64-bit
 let f1: Float = 100.3 // 32-bit
+let d3 = d1 + d2 + Double(f1) // convert it!
 
 // Switch statements
 
 // when an if/else statement gets complex, it is good practice to use a switch statement.
 
 var statusCode: Int = 405
-var errorString: String = "The request failed with the error:" // default statement for control transfer statement / fallthrough
+var errorString: String = "The request failed with the error:" // default statement for CONTROL TRANSFER STATEMENT / FALLTHROUGH
 
 switch statusCode {
 //case 400:
@@ -174,12 +170,12 @@ switch statusCode {
 // refactored code:
 case 400, 401, 403, 404:
 errorString = "there was something wrong with the request." // append string
-fallthrough // fallthrough keyword
+fallthrough // use fallthrough keyword...
 default:
     errorString += " please review the request and try again." // append string
 }
 
-// VERY COOL NON BOOK DISCOVERY. You can perform these functions on sets.
+// VERY COOL NON BOOK DISCOVERY!!! You can perform these functions on SETS.
 
 let array1 = ["a", "b", "c"]
 let array2 = ["a", "b", "d"]
@@ -187,7 +183,7 @@ let array2 = ["a", "b", "d"]
 let set1 = Set(array1) // create set
 let set2 = Set(array2)
 
-set1.union(set2)        // {"a", "b", "c", "d"}
+set1.union(set2)        // {"a", "b", "c", "d"} // not necessarily in this order...
 set1.intersect(set2)    // {"a", "b"}
 set1.subtract(set2)     // {"c"}
 set1.exclusiveOr(set2)  // {"c", "d"}
@@ -195,10 +191,10 @@ set1.exclusiveOr(set2)  // {"c", "d"}
 // RANGES! 
 
 // we can examine a RANGE of values with 1...10 (this includes both 1 and 10)
-// you can also add switch statement cases with a comma
+// you can also add multiple switch statement cases with a comma
 // value binding can be added to strings with \() syntax
 
-var secretCode: Int = 409
+var secretCode: Int = 204
 var errorMessage: String = "The request failed with an error:"
 
 switch secretCode {
@@ -210,13 +206,76 @@ case 300...307: // here is a range 300 to 307
     errorMessage += " Redirection, \(secretCode)"
 case 308...400:
     errorMessage += " Client error, \(secretCode)"
+case let unknownCode where (unknownCode >= 200 && unknownCode < 300) || unknownCode > 505: // create WHERE CLAUSE filter to catch extra conditions
+    errorMessage = "\(unknownCode) is not a known error code."
 default:
-    errorString = "\(secretCode) is not a known error code." // changing var string (no +=)
+    errorString = "Unknown error encountered." // changing var string (no +=)
 }
 
-// WHERE CLAUSES:
+// Tuples
 
-// the code above could be refactored
+//let error = (secretCode, errorMessage) // you can use tuples to group variables that are related - in one line!!
+//error.0 // you can access elements of a tuple by their index
+//error.1
+
+// but that's not very intuitive... we can name the tuple's elements...
+let error = (code: secretCode, error: errorMessage)
+error.code // and then call them by name!
+error.error
+
+// Pattern Matching
+
+let firstErrorCode = 404
+let secondErrorCode = 200
+let errorCodes = (firstErrorCode, secondErrorCode) // this is a tuple that binds the first and second error together. Cool!
+
+switch errorCodes {
+case (404, 404): // we search for tuples like this
+    print("no items found.")
+case (404, _): // the "_" is a wildcard that matches ANYTHING.
+    print("first item not found.")
+case (_, 404):
+    print("second item not found.")
+default:
+    print("all items found")
+}
+
+// Switch vs if/else
+
+// switch statements are useful for comparing a value against MULTIPLE potentially matching cases.
+// if/else statements are better used for checking against a SINGLE condition. 
+
+let age = 25
+if case 18...35 = age where age >= 21 { // WHOA: you can include a where clause in an if statement...
+    print("in a cool demographic and can shop at the wine store")
+}
+// only use this technique if you do not need a default case. 
+
+// CHALLENGE: 
+
+let point = (x: 1, y: 4)
+
+switch point {
+case let q1 where (point.x > 0) && (point.y > 0):
+    print("\(q1) is in quadrant 1")
+case let q2 where (point.x < 0) && (point.y > 0):
+    print("\(q2) is in quadrant 2")
+case let q3 where (point.x > 0) && (point.y < 0):
+    print("\(q3) is in quadrant 3")
+case let q4 where (point.x < 0) && (point.y < 0):
+    print("\(q4) is in quadrant 4")
+    default:
+    print("case not covered.")
+}
+// this maps out a graph with x, y coordinates and plots quadrant location! 1,4 would be in q1. 
+
+
+
+
+
+
+
+
 
 
 
