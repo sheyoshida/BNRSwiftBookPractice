03be515306ef71a65538bb8a31f5e20968a41c18
shenayoshida <sheyoshida@gmail.com>
2016-04-25 13:15:27 -0400

    tab bar controller

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -8,12 +8,10 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate { // conform to delegate, declare protocol
 
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
-    
-    
     
     var fahrenheitValue: Double? {
         didSet { // property observer is called whenever value changes
@@ -38,9 +36,61 @@ class ConversionViewController: UIViewController {
         return nf
     }() // adding closure to instantiate number formatter
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Conversion View Controller loaded its view.")
+        
+    }
+    
+    // silver challenge - make background color change based on time of day
+    override func viewWillAppear(animated: Bool) {
+        
+        // get the hour using NSCalendar
+        let hour = NSCalendar.currentCalendar().component(NSCalendarUnit.Hour, fromDate: NSDate())
+        
+        let myColor: UIColor
+        
+                switch hour {
+                case 1...6:
+                    myColor = UIColor.purpleColor()
+                case 7...12:
+                    myColor = UIColor.redColor()
+                case 13...18:
+                    myColor = UIColor.greenColor()
+                case 19...24:
+                    myColor = UIColor.yellowColor()
+                default:
+                    myColor = UIColor.lightGrayColor()
+                }
+        
+        view.backgroundColor = myColor
+
+    }
+    
+    // built in delegate method to prevent adding multiple decimal separators
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeparator = textField.text?.rangeOfString(".")
+        let replacementTextHasDecimalSeparator = string.rangeOfString(".")
+        let letterCharacters = NSCharacterSet.letterCharacterSet() // disallow letters
+        
+        if string.lowercaseString.rangeOfCharacterFromSet(letterCharacters) != nil {
+            return false
+        }
+        else {
+            if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
+                return false
+            }
+            else {
+                return true
+            }
+        }
+    }
+    
     func updateCelsiusLabel() {
+        
         if let value = celsiusValue {
-//            celsiusLabel.text = "\(value)"
             celsiusLabel.text = numberFormatter.stringFromNumber(value)
         }
         else {
---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Shena Yoshida on 4/25/16.
+//  Copyright © 2016 Shena Yoshida. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Map View Controller loaded its view.")
+        
+    }
+    
+}
